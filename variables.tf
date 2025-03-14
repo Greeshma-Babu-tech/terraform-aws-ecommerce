8511a37232e92ddd7e1d9c7735131672711b0b85
Greeshma Babu <141845293+Greeshma-Babu-tech@users.noreply.github.com>
2025-03-14 12:45:03 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,5 @@ variable "ecom_availability_zones" {
   description = "Availability Zones"
   default     = ["us-east-1a", "us-east-1b"]
 }
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
